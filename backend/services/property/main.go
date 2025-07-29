53ca115c613ffb2689c7a3c52217033fb6735b78
Tihe <tihechen@gmail.com>
2025-07-29 18:13:04 +0900

    delete sensitive secrets

--- backend/services/property/main.go
+++ backend/services/property/main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	// Database connection
-	dsn := "host=localhost user=postgres password=postgres dbname=susi port=5432 sslmode=disable"
+	dsn := "#TODO: using env variable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
